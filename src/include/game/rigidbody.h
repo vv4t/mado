166ea6773a699f2ebcc80d6132882834ebc70e10
Wilton Lu <watashidesu6969@gmail.com>
2024-05-22 18:14:27 +1000

    what i have rn

--- src/include/game/rigidbody.h
+++ src/include/game/rigidbody.h
@@ -1,8 +1,13 @@
 #ifndef RIGIDBODY_H
 #define RIGIDBODY_H
 
+typedef void (*map_hit_t)(struct game_s *gs, int e);
+// typedef void (*entity_hit_t)(struct game_s *gs, int e);
+
 typedef struct {
-  vector velocity;
+  vector        velocity;
+  map_hit_t     map_hit;
+  // entity_hit_t  entity_hit;
 } rigidbody_t;
 
 inline static rigidbody_t create_rigidbody()
