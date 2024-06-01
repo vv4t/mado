7e1b5f176cd18b35694ed9f94004f9603389b21d
vv4t <watashidesu6969@gmail.com>
2024-06-01 21:15:52 +1000

    added command line arg to tmxcvt

--- tools/tmxcvt/tmxcvt.js
+++ tools/tmxcvt/tmxcvt.js
@@ -3,26 +3,32 @@
 const { XMLParser, XMLBuilder, XMLValidator} = require("fast-xml-parser");
 const fs = require("fs");
 
-const opt = {
-  ignoreAttributes: false
-};
+if (process.argv.length != 3 && process.argv.length != 4) {
+  console.log("usage: tmxcvt.js in-tmx [out-map]");
+  process.exit(1);
+}
 
-const text = fs.readFileSync("saves/arena.tmx", { encoding: "utf8", flag: "r" });
-const xml = new XMLParser(opt).parse(text);
+const text = fs.readFileSync(process.argv[2], { encoding: "utf8", flag: "r" });
+const xml = new XMLParser({ ignoreAttributes: false }).parse(text);
 
 const width = parseInt(xml.map.layer["@_width"]);
 const height = parseInt(xml.map.layer["@_height"]);
 
-const raw_data = xml.map.layer.data["#text"].split(",").map((x) => parseInt(x).toString(16));
+const map_data = xml.map.layer.data["#text"].split(",").map((x) => parseInt(x).toString(16));
 
-console.log(width, height);
+const out = [];
 
-const data = [];
+out.push(width);
+out.push(height);
 
 for (let y = height - 1; y >= 0; y--) {
   for (let x = 0; x < width; x++) {
-    data.push(raw_data[y * width + x]);
+    out.push(map_data[y * width + x]);
   }
 }
 
-console.log(data.join(" "));
+if (process.argv.length == 4) {
+  fs.writeFileSync(process.argv[3], out.join(" "));
+} else {
+  console.log(out.join(" "));
+}
