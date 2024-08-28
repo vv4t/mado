65b9ced5a39b795d1312a6dfefbcbef08d3781d3
lexsai <thetruelexsai@gmail.com>
2024-08-28 10:19:31 +1000

    refactored conditions to improve how arguments are passed

--- src/include/game/conditions.h
+++ src/include/game/conditions.h
@@ -6,10 +6,10 @@
 
 typedef int entity_t;
 
-bool cond_greater_distance(game_t *gs, entity_t e, entity_t arg1, float arg2);
-bool cond_lesser_distance(game_t *gs, entity_t e, entity_t arg1, float arg2);
-bool cond_lesser_hp_flat(game_t *gs, entity_t e, entity_t arg1, float arg2);
-bool cond_lesser_hp_percent(game_t *gs, entity_t e, entity_t arg1, float arg2);
-bool cond_time_elapsed(game_t *gs, entity_t e, entity_t arg1, float arg2);
+condition_t cond_greater_distance(entity_t other, float dist);
+condition_t cond_lesser_distance(entity_t other, float dist);
+condition_t cond_lesser_hp_flat(float hp);
+condition_t cond_lesser_hp_percent(float hp);
+condition_t cond_time_elapsed(float delay);
 
 #endif
